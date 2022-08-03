755489bf9e81357e3d439de5bfaf3d332ee00ba6
ctnohilly <ctnohilly@gmail.com>
2022-08-03 01:04:04 -0400

    created delete product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,7 +28,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findAll({
+  Product.findOne({
     where: {
       id: req.params.id
     },
@@ -41,7 +41,13 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
-    .then(dbProductData => res.json(dbProductData))
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -124,6 +130,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
